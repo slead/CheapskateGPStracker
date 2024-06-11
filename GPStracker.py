af905413db9c6148fd04b54301e0ff4fe5a89e3f
Stephen.Lead <Stephen.Lead@transport.nsw.gov.au>
2024-06-11 20:39:52 +1000

    simplify code, allow user to choose the device to track

--- GPStracker.py
+++ GPStracker.py
@@ -7,9 +7,12 @@ import os
 from math import radians, sin, cos, sqrt, atan2
 import json
 
-file_path = "/Users/steve/Dropbox/shitbox rally/tracking/tracks.csv"
+file_path = "/Users/steve/Dropbox/Apps/ShitboxRallyTracker/points.csv"
 
 def calculate_distance(lat1, lon1, lat2, lon2):
+    # Don't fill up the database with duplicate points - only update
+    # if the device has moved by more than the speficied distance
+
     # Radius of the Earth in km
     R = 6371.0
 
@@ -36,64 +39,47 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 def tracker(iphone, prev_longitude, prev_latitude, firstRun:bool):
     
     try:
-        if is_daylight_hours():
-            current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            location = iphone.location()
-            timestamp = location['timeStamp'] #datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            latitude = location['latitude']
-            longitude = location['longitude']
+        current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        location = iphone.location()
+        timestamp = location['timeStamp'] #datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        latitude = location['latitude']
+        longitude = location['longitude']
 
-            if prev_latitude == 0:
-                prev_latitude = latitude
-            if prev_longitude == 0:
-                prev_longitude = longitude
+        if prev_latitude == 0:
+            prev_latitude = latitude
+        if prev_longitude == 0:
+            prev_longitude = longitude
 
-            # check whether the position has moved enough to track
-            distance_threshold = 1  # metres
-            if firstRun or calculate_distance(prev_latitude, prev_longitude, latitude, longitude) >= distance_threshold:
-                prev_longitude = longitude
-                prev_latitude = latitude
-                data = [current_time,timestamp, latitude, longitude]
-                print(data)
+        # check whether the position has moved enough to track
+        distance_threshold = 10  # metres
+        if firstRun or calculate_distance(prev_latitude, prev_longitude, latitude, longitude) >= distance_threshold:
+            prev_longitude = longitude
+            prev_latitude = latitude
+            data = [current_time,timestamp, latitude, longitude]
+            print(data)
 
-                # write to file
-                with open(file_path, 'a', newline='\n') as csvfile:
-                    # Create a CSV writer object
-                    csv_writer = csv.writer(csvfile)
-                
-                    # Append the data to the CSV file
-                    csv_writer.writerow(data)
-                
-                # Sleep then run again
-                t.sleep(600)
-                tracker(iphone, prev_longitude, prev_latitude, False)
-            else:
-                print("Point has not moved by more than 50 meters.")
-                t.sleep(600)
-                tracker(iphone, prev_longitude, prev_latitude, False)
+            # Append the data to the CSV file
+            with open(file_path, 'a', newline='\n') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(data)
 
-        else: 
+            # TODO: also store in a more robust location, eg Firebase or Postgres
+            
+            # Sleep then run again
             t.sleep(600)
+            tracker(iphone, prev_longitude, prev_latitude, False)
+        else:
+            print(f"Point has not moved by more than {distance_threshold} meters.")
+            t.sleep(600)
+            tracker(iphone, prev_longitude, prev_latitude, False)
+
     except Exception as e:
         print("There was a problem, trying again in 30 seconds")
         print(e.args)
         t.sleep(30)
         tracker(iphone, prev_longitude, prev_latitude, False)
 
-def is_daylight_hours():
-    return True
-
-    # Get the current local time
-    current_time = dt.datetime.now().time()
-
-    # Define the time boundaries (8am and 6pm)
-    start_time = dt.time(6, 0, 0)   # 6:00:00 AM
-    end_time = dt.time(20, 0, 0)    # 8:00:00 PM
-
-    # Check if the current time is between the start and end time
-    return start_time <= current_time <= end_time
-
-# Read from config.json
+# Read the iCloud email and password from config.json
 with open('config.json', 'r') as file:
     config = json.load(file)
     email = config['email']
@@ -140,11 +126,14 @@ elif api.requires_2sa:
         print("Failed to verify verification code")
         sys.exit(1)
 
-if not os.path.isfile(file_path):
-    with open(file_path, 'w', newline='\n') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['CurrentTime', 'Timestamp', 'Latitude', 'Longitude'])
-iphone = api.devices[1]
+# Figure out which devices are accessible to Find My iPhone
+devices = api.devices
+for index, device in enumerate(devices):
+    print(index, device)
+
+# Choose a device based on user input
+device_index = int(input("Enter the index of the iPhone you want to track: "))
+iphone = devices[device_index]
 
 # keep track of the last known position
 prev_latitude = 0
